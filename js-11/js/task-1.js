36a8dcdd4f6bab2e5ab96abbd6d8d4393d690830
Alexandr Kolisetsky <skolisetsky@yahoo.com>
2019-11-01 11:05:01 +0200

    -11

--- js-11/js/task-1.js
+++ js-11/js/task-1.js
@@ -1,14 +1,44 @@
 "use strict";
+// Напиши скрипт, который после нажатия кнопки Start, раз в секунду
+// меняет цвет фона body на случайное значение из массива используя
+// инлайн-стиль. При нажатии на кнопку Stop, изменение цвета фона
+// должно останавливаться.
+// ⚠️ Учти, на кнопку Start можно нажать бесконечное количество раз.
+// Сделай так, чтобы пока изменение темы запушено, кнопка Start была не
+// активна.
+// Для генерации случайного числа (индекс элемента массива цветов),
+// используй функцию randomIntegerFromInterval.
 
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548"
+];
 
+const start = document.querySelector('[data-action="start"]');
+const stop = document.querySelector('[data-action="stop"]');
+const body = document.querySelector("body");
+let timer;
+stop.disabled = true;
 
+start.addEventListener("click", () => {
+  stop.disabled = false;
+  start.disabled = true;
 
+  timer = setInterval(() => {
+    const randomIntegerFromInterval = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+});
 
-// const colors = [
-//     '#FFFFFF',
-//     '#2196F3',
-//     '#4CAF50',
-//     '#FF9800',
-//     '#009688',
-//     '#795548',
-//   ];
+stop.addEventListener("click", () => {
+  clearInterval(timer);
+  start.disabled = false;
+  stop.disabled = true;
+});
--- js-11/js/task-3.js
+++ js-11/js/task-3.js
@@ -1,42 +1,90 @@
 "use strict";
 
-new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019")
-});
+// Создай плагин настраиваемого таймера, который ведет обратный отсчет
+// до предварительно определенной даты. Такой плагин может использоваться
+// в блогах и интернет-магазинах, страницах регистрации событий,
+// во время технического обслуживания и т. д.
+
+// Плагин ожидает следующую HTML-разметку и показывает четыре
+// цифры: дни, часы, минуты и секунды в формате XX:XX:XX:XX. Количество
+// дней может состоять из более чем двух цифр.
+
+// Плагин это класс CountdownTimer, экземпляр которого создает новый
+// таймер с настройками.
+// Для подсчета значений используй следующие готовые формулы,
+// где time - разница между targetDate и текущей датой.
+
+// Создай плагин настраиваемого таймера, который ведет обратный отсчет до предварительно определенной даты. Такой плагин может использоваться в блогах и интернет-магазинах, страницах регистрации событий, во время технического обслуживания и т. д.
+
+// Плагин ожидает следующую HTML-разметку и показывает четыре цифры: дни, часы, минуты и секунды в формате XX:XX:XX:XX. Количество дней может состоять из более чем двух цифр.
+
+// Плагин это класс CountdownTimer, экземпляр которого создает новый таймер с настройками.
 
 class CountdownTimer {
-  constructor({ selector, target }) {
-    this.selector = docoment.queryselector("${selector}");
-    this.target = targetDate.getTime();
-  }
-  getInfo() {
-    setInterval(() => {
-      const currentTime = Date.now();
-      const time = this.target - this.currentTime;
-    }, 1000);
+  constructor(name, date) {
+    this._selector = name;
+    this._targetDate = new Date(date);
   }
 }
+
+CountdownTimer.prototype.changeTimerValue = function() {
+  setInterval(() => {
+    const time = this._targetDate - Date.now();
+    if (time <= 0) {
+      return;
+    }
+
+    const timer = document.querySelector(`${this._selector}`);
+    const daysValue = timer.querySelector('[data-value="days"]');
+    const hoursValue = timer.querySelector('[data-value="hours"]');
+    const minsValue = timer.querySelector('[data-value="mins"]');
+    const secsValue = timer.querySelector('[data-value="secs"]');
+
+    const days = String(Math.floor(time / (1000 * 60 * 60 * 24))).padStart(
+      2,
+      "0"
+    );
+    const hours = String(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    ).padStart(2, "0");
+    const mins = String(
+      Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+    ).padStart(2, "0");
+    const secs = String(Math.floor((time % (1000 * 60)) / 1000)).padStart(
+      2,
+      "0"
+    );
+    daysValue.textContent = days;
+    hoursValue.textContent = hours;
+    minsValue.textContent = mins;
+    secsValue.textContent = secs;
+
+    // console.log(mins, secs);
+  }, 1000);
+};
+
+const newTimer = new CountdownTimer("#timer-1", "Nov 1, 2019");
+console.log(newTimer);
+newTimer.changeTimerValue();
+const timer2 = new CountdownTimer("#timer-2", "Oct 30, 2019");
+timer2.changeTimerValue();
 
 /*
  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
  */
 const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
 /*
  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
  * остатка % и делим его на количество миллисекунд в одном часе
  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
  */
 const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
 /*
  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
  */
 const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
 /*
  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
  * миллисекунд в одной секунде (1000)
